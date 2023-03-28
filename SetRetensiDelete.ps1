167945ac4ca4aee181c91255c65180da187cadc1
goms12 <63918976+goms12@users.noreply.github.com>
2023-03-28 10:22:43 +0700

    Create SetRetensiDelete.ps1

---
+++ SetRetensiDelete.ps1
@@ -0,0 +1,34 @@
+function SetRetensiDeletes
+{
+    Param
+    (
+        [String]$source, 
+        [int]$p_retensi, 
+        [String]$format_tanggal
+     )
+
+# $format_tanggal = 'yyyy_MM_dd dan yyyy_MM'
+
+    # Create file if not exist
+    if (!(test-path D:\Database\log_backup_$(get-date -Format "yyyy_MM").log -PathType Leaf))
+    {
+        New-Item -Path D:\Database\log_backup_$(get-date -Format "yyyy_MM").log
+    }
+    #Just condition for me because run this script on every friday.
+    if ((Get-Date).DayOfWeek -eq 'Friday')
+    {
+        for($i=0; $i -le 31; $i++)
+        {
+            $delete_pattern = (Get-Date).AddDays(-$p_retensi - $i).ToString($format_tanggal)
+            $p_file = Get-ChildItem $source -Recurse -Filter *$delete_pattern*
+            if($null -ne $p_file)
+            {
+                ForEach($x_file in $p_file)
+                {
+                    Remove-Item $x_file.FullName
+                    Add-Content D:\Database\log_backup_$(get-date -Format "yyyy_MM").log -Value "`n $(Get-Date -Format yyyy-MM-dd)|$(Get-Date -UFormat %T)|Deleted|$($x_file.FullName)"
+                }
+            }
+        }
+    }
+}
